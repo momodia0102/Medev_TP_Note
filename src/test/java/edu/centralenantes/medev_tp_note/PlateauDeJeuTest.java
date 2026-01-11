9166d5b16b5c898765db67da5724c3ae0f405fbe
momodia0102 <m2000dia@gmail.com>
2026-01-11 13:20:09 +0100

    first commit

---
+++ src/test/java/edu/centralenantes/medev_tp_note/PlateauDeJeuTest.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package edu.centralenantes.medev_tp_note;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+/**
+ *
+ * @author Mohamed
+ */
+
+
+public class PlateauDeJeuTest {
+
+    @Test
+    public void testPlacementValide() {
+        PlateauDeJeu plateau = new PlateauDeJeu(5, 5);
+        boolean resultat = plateau.placer(2, 2, 'X');
+        assertTrue(resultat, "Le placement devrait être valide");
+    }
+
+    @Test
+    public void testPlacementSurCaseOccupee() {
+        PlateauDeJeu plateau = new PlateauDeJeu(5, 5);
+        plateau.placer(1, 1, 'X');
+        boolean resultat = plateau.placer(1, 1, 'O');
+        assertFalse(resultat, "Impossible de placer sur une case occupée");
+    }
+
+    @Test
+    public void testPlacementHorsPlateau() {
+        PlateauDeJeu plateau = new PlateauDeJeu(5, 5);
+        boolean resultat = plateau.placer(10, 0, 'X');
+        assertFalse(resultat, "Impossible de placer hors du plateau");
+    }
+}
